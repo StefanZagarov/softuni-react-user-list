0671f19444f89cc2c9b946d026b5f6a0a3bcff2e
StefanZagarov <stefanzagarov@gmail.com>
2025-03-02 14:37:05 +0200

    Created user service and function to get all users

---
+++ client/src/services/userService.js
@@ -0,0 +1,16 @@
+const baseUrl = 'http://localhost:3030/jsonstore/users';
+
+export default {
+    async getAll() {
+        try {
+            const response = await fetch(baseUrl);
+            const result = await response.json();
+            const users = Object.values(result);
+
+            return users;
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+};
